f1d5e13dab3e232ad1240e90c8f8bfbdd2c1c86a
Alaettin UÇAN <ucanalaettin@gmail.com>
2022-11-03 13:57:59 +0300

    Add files via upload

--- service.py
+++ service.py
@@ -3,12 +3,15 @@ Numara:
 Ad Soyad:
 """
 
- 
+
+from datetime import date
 class Service:
     """Tamir servisi işlemlerini yapan sınıf"""
 
     """işçilik ücreti oranını ve servisin adını class variable olarak tanımlayıp, 
-    ilk değerlerini giriniz. """ 
+    ilk değerlerini giriniz. """
+    workmanship_rate:float = 0.15
+    service_name:str = "Osman Usta"
 
 
     """ price_list dictionary'si class variable olarak tanımlanmıştır
@@ -37,9 +40,13 @@ class Service:
             name_surname (str): isim soyisim
             work_list (list[str]): yapılacak iş listesi
         """
-        
+        self.plate_no = plate_no
+        self.name_surname = name_surname
+        self.work_list = work_list
+        self.__work_price = []
+        self.total = 0
 
-   
+    @classmethod
     def onbin(cls,plate_no:str, name_surname:str, work_list:list[str]=[]):
         """onbin bakımı için ön tanımlı iş listesine sahip constructor overload
 
@@ -52,9 +59,10 @@ class Service:
         Returns:
             Service: geriye service türünde bir örnek döndürüyor
         """
-        
+        work_list.extend(["yag","yag_filtresi","hava_filtresi","polen_filtresi","yikama"])
+        return cls(plate_no,name_surname,work_list)
     
-    
+    @classmethod
     def yirmibin(cls,plate_no:str, name_surname:str, work_list:list[str]=[]):
         """yirmibin bakımı için ön tanımlı iş listesine sahip constructor overload
 
@@ -66,9 +74,11 @@ class Service:
         ön tanımlı iş listesi["motor_temizligi","balata","disk","yag","yag_filtresi","hava_filtresi","polen_filtresi","yikama"]
         Returns:
             Service: geriye service türünde bir örnek döndürüyor
-        """ 
+        """
+        work_list = ["motor_temizligi","balata","disk"]
+        return cls.onbin(plate_no,name_surname,work_list)
     
-    
+    @classmethod
     def otuzbin(cls,plate_no:str, name_surname:str, work_list:list[str]=[]):
         """otuzbin bakımı için ön tanımlı iş listesine sahip constructor overload
 
@@ -80,47 +90,58 @@ class Service:
         ön tanımlı iş listesi["triger","buji","motor_temizligi","balata","disk","yag","yag_filtresi","hava_filtresi","polen_filtresi","yikama"]
         Returns:
             Service: geriye service türünde bir örnek döndürüyor
-        """ 
+        """
+        work_list = ["triger","buji"]
+        return cls.yirmibin(plate_no,name_surname,work_list)
     
-    
+    @property
     def name_surname(self) -> str:
         """name_surname getter
 
         Returns:
             str: __name ve __surname gizli değişkenlerinde tutulan anonimleştirilmiş isim ve soy ismi geriye döndürür
             örnek: Ala*** Uça***
-        """ 
+        """
+        return f'{self.anonymize(self.__name)} {self.anonymize(self.__surname)}'
 
-    
+    @name_surname.setter
     def name_surname(self,value:str) -> None:
         """name_surname setter
 
         Args:
             value (str): içerisine isim ve soyisim alıyor ve __name ve __surname gizli değişkenlerine atama yapıyor.
-        """ 
+        """
+        self.__name,self.__surname = value.split()
 
-    
+    @classmethod
     def set_workmanship_rate(cls,workmanship_rate:float) -> None:
         """işçilik oranı class variable ını değiştiren class method
 
         Args:
             workmanship_rate (float): işçilik oranı
-        """ 
+        """
+        cls.workmanship_rate = workmanship_rate
 
-    
+    @classmethod
     def set_service_name(cls,service_name) -> None:
         """servis adı class variable nı değiştiren class method
 
         Args:
             service_name (_type_): servis adı 
-        """ 
+        """
+        cls.service_name = service_name
 
     def do_worklist(self):
         """iş listesinde yer alan her işçilik kalemi için
            önce işin fiyatını bulur
            sonra işçilik oranına göre işçilik ücretini hesaplar
            yapılan işler listesine ekler
-        """ 
+        """
+        for work in self.work_list:
+            price = self.price_list[work]
+            workmanship = price * self.workmanship_rate
+            self.total += price + workmanship
+            self.__work_price.append(f'*{work},{price}tl,{workmanship}tl')
 
     def print(self) -> None:
         """serviste yapılan işlerin ücret özetini ekrana basar
@@ -132,10 +153,18 @@ class Service:
             Toplam: <toplam_tutar>tl
             Servis: <service_name>
             20 tane -
-        """ 
+        """
+        print('-' * 20)
+        print(f'{self.plate_no}-{self.name_surname}-{date.today()}')
+        for wp in self.__work_price:
+            print(wp)
+        print(f'İşçilik Oranı:{self.workmanship_rate}')
+        print(f'Toplam:{self.total}tl')
+        print(f'Servis:{self.service_name}')
+        print('-' * 20)
         
 
-    
+    @staticmethod
     def anonymize(_in) -> str:
         """Gelen stringin ilk üç harfini alır ve 
         sonuna *** ekleyerek anonimleştirme işlemi gerçekleştirir.
@@ -146,4 +175,7 @@ class Service:
 
         Returns:
             str: anonimleşmiş string
-        """ 
+        """
+        _out:str = ""
+        _out = _in[:3]+"***"
+        return _out
